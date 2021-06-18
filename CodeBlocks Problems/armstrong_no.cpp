503d6a5cadbc18ba821a469535aae0d65bf3c775
Akshat <akshatsahu100@gmail.com>
2021-06-18 18:59:52 +0530

    Hemant's code for armstrong number

---
+++ CodeBlocks Problems/armstrong_no.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vp vector<pair<int,int>> 
+#define vvp vector<vector<pair<int,int>>> 
+#define vc vector<char>
+ll MOD = 1e9 + 7;
+double pi = 3.14159;
+void inp_vector(vi &a){ for(int i=0; i<(int)a.size(); i++){ cin>>a[i]; }}
+void disp_vector(vi &a){ for(int i=0; i<(int)a.size(); i++){ cout<<a[i]<<" ";  }}
+void disp_vvi(vvi &vec){ for(int i=0; i<(int)vec.size(); i++){ for(int j=0; j<(int)vec[i].size(); j++) cout<<vec[i][j]<<" "; cout<<endl;}}
+void inp_vvi(vvi &vec){ for(int i=0; i<(int)vec.size(); i++) for(int j=0; j<(int)vec[i].size(); j++) cin>>vec[i][j]; }
+
+int main(){
+     int num,temp,len = 0, sum = 0;
+
+     cin>>num; // keeps my original value intact
+     temp = num; //change always
+     while(temp){
+          temp = temp/10;
+          len++;
+     }
+     //temp = 0
+     temp = num;
+     while(temp){
+          int rem = temp%10;
+          temp = temp/10;
+          sum += pow(rem,len);
+     }
+     // temp = 0
+     if(sum == num) cout<<"true";
+     else cout<<"false";
+
+}
