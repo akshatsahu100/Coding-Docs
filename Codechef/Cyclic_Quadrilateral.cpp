b77d417b5521887c7622d465c13ee66e3a03bf07
Akshat <akshatsahu100@gmail.com>
2021-06-27 17:13:16 +0530

    CodeChef Starters contest june 2021

---
+++ Codechef/Cyclic_Quadrilateral.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vp vector<pair<int,int>> 
+#define vvp vector<vector<pair<int,int>>> 
+#define vc vector<char>
+ll MOD = 1e9 + 7;
+double pi = 3.14159;
+void inp_vector(vi &a){ for(int i=0; i<(int)a.size(); i++){ cin>>a[i]; }}
+void disp_vector(vi &a){ for(int i=0; i<(int)a.size(); i++){ cout<<a[i]<<" ";  }}
+void disp_vvi(vvi &vec){ for(int i=0; i<(int)vec.size(); i++){ for(int j=0; j<(int)vec[i].size(); j++) cout<<vec[i][j]<<" "; cout<<endl;}}
+void inp_vvi(vvi &vec){ for(int i=0; i<(int)vec.size(); i++) for(int j=0; j<(int)vec[i].size(); j++) cin>>vec[i][j]; }
+
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+          int a,b,c,d;
+          cin>>a>>b>>c>>d;
+          if(a + c == 180 and b + d == 180)
+               cout<<"YES"<<endl;
+          else 
+               cout<<"NO"<<endl;
+     }
+}
---
+++ Codechef/Sleep_Cycle.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vi vector<int>
+#define vb vector<bool>
+#define vvi vector<vector<int>>
+#define vp vector<pair<int,int>> 
+#define vvp vector<vector<pair<int,int>>> 
+#define vc vector<char>
+ll MOD = 1e9 + 7;
+double pi = 3.14159;
+void inp_vector(vi &a){ for(int i=0; i<(int)a.size(); i++){ cin>>a[i]; }}
+void disp_vector(vi &a){ for(int i=0; i<(int)a.size(); i++){ cout<<a[i]<<" ";  }}
+void disp_vvi(vvi &vec){ for(int i=0; i<(int)vec.size(); i++){ for(int j=0; j<(int)vec[i].size(); j++) cout<<vec[i][j]<<" "; cout<<endl;}}
+void inp_vvi(vvi &vec){ for(int i=0; i<(int)vec.size(); i++) for(int j=0; j<(int)vec[i].size(); j++) cin>>vec[i][j]; }
+
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+          int l,h;
+          string str;
+          cin>>l>>h;
+          cin>>str;
+          int temp = 0; 
+          bool flag = false;
+
+          for(int i=0; i<str.size(); i++){
+               if(str[i] == '0'){
+                    temp++;
+               }
+               else{
+                    if(temp == 0) continue;
+                    int x = h - temp;
+                    if(x <= 0){
+                         flag = true;
+                         break;
+                    }
+                    if(2*x < h)
+                         h = 2 * x;
+                    temp = 0;
+               }
+
+               if(i == str.size() - 1){
+                    int x = h - temp;
+                    if(x <= 0){
+                         flag = true;
+                         break;
+                    }
+               }
+          }
+          if(flag)
+          cout<<"YES"<<endl;
+          else 
+          cout<<"NO"<<endl;
+     }
+}
